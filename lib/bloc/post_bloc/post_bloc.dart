11ef6ed93dc259d44f274a869faf5eb052bf30c7
Asif Taj <axiftaj@gmail.com>
2024-03-19 19:26:40 +0500

    search bug fixed

--- lib/bloc/post_bloc/post_bloc.dart
+++ lib/bloc/post_bloc/post_bloc.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:bloc/bloc.dart';
 import 'package:bloc_tutorials/bloc/post_bloc/post_event.dart';
 import 'package:bloc_tutorials/bloc/post_bloc/post_state.dart';
@@ -7,49 +5,34 @@ import 'package:bloc_tutorials/repository/post_repository.dart';
 
 import '../../model/post_model.dart';
 
-class PostBloc extends Bloc<PostEvent , PostState> {
-  PostRepository postRepository  = PostRepository();
-   List<PostModel> tempPostList = [];
+class PostBloc extends Bloc<PostEvent, PostState> {
+  PostRepository postRepository = PostRepository();
+  List<PostModel> tempPostList = [];
 
-  PostBloc() :super(const PostState()){
+  PostBloc() : super(const PostState()) {
     on<PostFetched>(fetchPostApi);
     on<SearchItem>(_searchItem);
   }
 
-  Future<void> fetchPostApi(PostFetched event, Emitter<PostState> emit)async {
-
-    await postRepository.fetchPost().then((value){
-      emit(
-          state.copyWith(
-              status: PostStatus.success ,
-              postsList: value ,
-              message: 'success'
-          )
-      );
-    }).onError((error, stackTrace){
-      emit(
-          state.copyWith(
-              status: PostStatus.failure ,
-              message: error.toString()
-          )
-      );
+  Future<void> fetchPostApi(PostFetched event, Emitter<PostState> emit) async {
+    await postRepository.fetchPost().then((value) {
+      emit(state.copyWith(status: PostStatus.success, postsList: value, message: 'success'));
+    }).onError((error, stackTrace) {
+      emit(state.copyWith(status: PostStatus.failure, message: error.toString()));
     });
   }
 
   void _searchItem(SearchItem event, Emitter<PostState> emit) {
-
-    if(event.stSearch.isEmpty){
-      emit(state.copyWith(tempSearchPostList: [] , searchMessage: ''));
-    }else {
-      tempPostList = state.posts.where((element) =>  element.title.toString().toLowerCase().toString().contains(event.stSearch.toString().toLowerCase())).toList();
+    if (event.stSearch.isEmpty) {
+      emit(state.copyWith(tempSearchPostList: [], searchMessage: ''));
+    } else {
+      tempPostList = state.posts.where((element) => element.title.toString().toLowerCase().toString().contains(event.stSearch.toString().toLowerCase())).toList();
       // tempPostList = state.posts.where((map)=>map.userId.toString() == event.stSearch).toList();
-      if(tempPostList.isEmpty){
-        emit(state.copyWith(tempSearchPostList: tempPostList , searchMessage: 'No data found'));
-      }else {
-        emit(state.copyWith(tempSearchPostList: tempPostList  ));
+      if (tempPostList.isEmpty) {
+        emit(state.copyWith(tempSearchPostList: tempPostList, searchMessage: 'No data found'));
+      } else {
+        emit(state.copyWith(tempSearchPostList: tempPostList, searchMessage: ''));
       }
     }
-
   }
-
 }
